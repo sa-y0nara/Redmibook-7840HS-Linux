7ca4ae5e66cd35d2d4e839c99744a241c57d1833
SAYONARA <149249833+sa-y0nara@users.noreply.github.com>
2023-11-02 10:57:36 +0800

    The scripts to install Redeon 780M driver.

---
+++ Radeon_780_driver/Redeon_780M_driver_installer.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+#驱动来源：https://www.amd.com/zh-hans/support/linux-drivers
+#Revision Number 23.20
+
+wget https://repo.radeon.com/amdgpu-install/23.20/ubuntu/jammy/amdgpu-install_5.7.50700-1_all.deb
+sudo apt install amdgpu-install_5.7.50700-1_all.deb

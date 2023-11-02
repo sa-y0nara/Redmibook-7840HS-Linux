7fb29696674a3990ec9d89959cfa2a72220e3d04
SAYONARA <149249833+sa-y0nara@users.noreply.github.com>
2023-11-02 11:02:03 +0800

    Delete Radeon_780M_driver_with_ROCM.sh

--- Radeon_780M_driver_with_ROCM.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#驱动来源：https://www.amd.com/zh-hans/support/linux-drivers，版本为23.20.00.48。
-
-sudo apt update
-wget https://repo.radeon.com/amdgpu-install/23.20.00.48/ubuntu/jammy/amdgpu-install_5.7.00.48.50700-1_all.deb
-sudo apt install ./amdgpu-install_5.7.00.48.50700-1_all.deb
-sudo amdgpu-install -y --usecase=graphics,rocm
-sudo usermod -a -G render,video $LOGNAME
